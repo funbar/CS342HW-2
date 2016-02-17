875940a40942d0f5c3596a02277e3a39aa1730e1
Richard Kim <rkim43@uic.edu>
2016-02-17 13:18:57 -0600

    Commit
    Added more methods in game class

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/ab/50577d4fa6d500151639a2132ba7e575
@@ -0,0 +1,19 @@
+import javax.swing.JButton;
+
+public class Game {
+	// there are 10 mines in our game every time, so we need to keep track of this.
+	private int mines = 10;
+	// I feel as if the timer should be a float, but we'll keep it as an int for now.
+	private int timer = 0;
+	
+	// Declaring a 2d array for our JButtons.
+	private JButton grid[][];
+	
+	// Constructor for our Game class.
+	public Game()
+	{
+		
+	}
+	
+	
+}
--- Minesweeper/src/Game.java
+++ Minesweeper/src/Game.java
@@ -1,10 +1,16 @@
-import javax.swing.JButton;
+import java.awt.GridLayout;
 
-public class Game {
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class Game extends JPanel{
 	// there are 10 mines in our game every time, so we need to keep track of this.
-	private int mines = 10;
+	private int mines_left = 10;
 	// I feel as if the timer should be a float, but we'll keep it as an int for now.
 	private int timer = 0;
+	
+	private final int rows    = 10;
+	private final int columns = 10;
 	
 	// Declaring a 2d array for our JButtons.
 	private JButton grid[][];
@@ -12,7 +18,62 @@ public class Game {
 	// Constructor for our Game class.
 	public Game()
 	{
+		JPanel score_board = new JPanel(new GridLayout(2, 3, 5, 3));
+		grid = new JButton[rows][columns];
 		
+		/*
+		 * use new ImageIcon ("image path in project") to add pictures instead of text
+		 */
+		
+		
+	}
+	
+	private void build_grid()
+	{
+		for(int i = 0; i<rows; i++)
+		{
+			for(int j = 0; j<columns; j++)
+			{
+				grid[i][j] = new JButton();
+				
+			}
+		}
+	}
+	
+	private void menu_bar()
+	{
+		/*
+		 * TODO new JMenu object instance here.
+		 */
+	}
+	
+	private void generate_mines()
+	{
+		/*
+		 * create mines for the games
+		 */
+	}
+	
+	private void randomizer()
+	{
+		/*
+		 * probably not necessary, but if we need a randomized method outside of the traditional method, 
+		 * we can implement out own. Used for randomly placing mines, etc.
+		 */
+	}
+	
+	private void top_scores()
+	{
+		/*
+		 * List of top scores - top 10
+		 */
+	}
+	
+	private void reset()
+	{
+		/*
+		 * TODO: we'll need to reset both the game and also the top scores at one point.
+		 */
 	}
 	
 	
