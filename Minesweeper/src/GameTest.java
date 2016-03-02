badac8392c5ca8cdf359cab3d1d3a5ebfe648500
Richard Kim <rkim43@uic.edu>
2016-03-01 21:29:41 -0600

    Update GameTest.java

--- Minesweeper/src/GameTest.java
+++ Minesweeper/src/GameTest.java
@@ -1,9 +1,16 @@
 import static org.junit.Assert.*;
 
+import java.util.Timer;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 import org.junit.Test;
 
-public class GameTest {
+public class GameTest extends Game {
 
+	Game gameObj = new Game();
+	
 	@Test
 	public void getMineLeftTest() {
 		Game T1 = new Game();
@@ -20,6 +27,51 @@ public class GameTest {
 	    assertEquals(6, T2.mines_left);
 	}
 		
+	@Test
+	public void resetTimer(Timer timer)
+	{
+		int time = gameObj.getTimer();
+		assertEquals(0, time);
+	}
 
+	@Test
+	public void testGameInput(String username){
+		
+		// test case for if the user doesn't enter a name
+		assertNotNull(username);
+		
+	}
+	
+	@Test
+	public void testGameInputMax(String username)
+	{
+		// test case for is the user enters in a string that is too long.
+		assertTrue(username.length() < 50);
+	}
+	
+	
+	@Test
+	public void testMouseClickedWhenGameEnds(MouseEvent arg0)
+	{
+		gameObj.gameOver();
+		if(gameObj.mouseClicked(arg0) == 0)
+		{
+			if(arg0.getSource() == JButton)
+			{
+				assertFalse("Game is not over", false);
+			}
+		}
+	}
+	
+	@Test
+	public void gameWinnerTest(JOptionPane jPane)
+	{
+		String winner = "You win!";
+	//	JOptionPane.showMessageDialog(null, "You win!");
+		inputTest = (JTextField)TestUtils(jPane, "You win!");
+		assertEquals(winner, inputTest);
+		
+		
+	}
 
 }
